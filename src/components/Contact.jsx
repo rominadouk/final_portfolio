a5ac8a5ac4a48828421cdd543fafc3de53c53e54
rominadouk <douk.romina@gmail.com>
2023-04-07 02:14:22 -0700

    adjusted title and descriptions

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
 import emailjs from '@emailjs/browser';
+import './Contact.css'
 
 
 const ContactForm = () => {
@@ -17,35 +19,37 @@ const ContactForm = () => {
       });
   };
     return (  
-        <>
-        <div className="contactForm">
+        <div id="contactForm">
             <div className='solid-line'></div>
-            <div className="container mt-5 mb-5">
-                <h2 className="mb-3">Contact Me</h2>
-                <form ref={form} onSubmit={sendEmail}>
-                    <div className="mb-3">
-                    <label className="form-label" htmlFor="name">
-                        Name
-                    </label>
-                    <input className="contactField form-control" type="text" id="name" name="user_name" required />
-                    </div>
-                    <div className="mb-3">
-                    <label className="form-label" htmlFor="email">
-                        Email
-                    </label>
-                    <input className="contactField form-control" type="email" id="email"  name="user_email" required />
-                    </div>
-                    <div className="mb-3">
-                    <label className="form-label" htmlFor="message">
-                        Message
-                    </label>
-                    <textarea className="contactField form-control" id="message" name="message" required />
-                    </div>
-                    <input type="submit" value="Send"/>
-                </form>
-            </div>
+            <Container className='my-5'>
+                <Row>
+                    <Col className="offset-sm-2 col-sm-8 offset-md-3 col-md-6">
+                        <h2 className="mb-3">Contact Me</h2>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="name">
+                                    Name
+                                </label>
+                                <input className="contactField form-control" type="text" id="name" name="user_name" required />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="email">
+                                    Email
+                                </label>
+                                <input className="contactField form-control" type="email" id="email"  name="user_email" required />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label" htmlFor="message">
+                                    Message
+                                </label>
+                                <textarea className="contactField form-control" id="message" name="message" required />
+                            </div>
+                            <input type="submit" value="Send"/>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
         </div>
-        </>
     );
 }
  
