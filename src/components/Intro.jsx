8fe0ef89f24b4d6b74251789ca6015c45f623f51
rominadouk <douk.romina@gmail.com>
2023-04-27 14:24:23 -0700

    intro stacks on mobile, with bootstrap

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,6 @@
 import './Intro.css'
 import { Container, Row, Col } from 'react-bootstrap';
-import Icon from './Icon';
+import Icon from '../assets/mega-creator (1).svg'
 
 const Intro = () => {
     return ( 
@@ -8,16 +8,22 @@ const Intro = () => {
         <div className='px-3' id='intro'>
             <Container id='intro-container'>
                 <Row>
-                    <Col>
-                        <h1 className='display-1 headers'><strong>Hi! I'm <span id='romina'>Romina.</span></strong></h1>
-                        <p className="display-6 headers">A Software Engineer.</p>
+                    <Col className='col-12 col-xl-4'>
+                        <h1 className='display-1 headers'><strong>I'm <span id='romina'>Romina.</span></strong></h1>
+                        <p className="display-6 headers">A Software Engineer</p>
                         <p className="intro-text">I enjoy creating efficient and reliable software.<br/> 
                         Let's build something amazing together!</p>
                     </Col>
+                    <Col className='col-12 col-xl-8'>
+                    <img id='icon' src={Icon} alt="Logo" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className='col-12'>
+                    <div id='intro-line' className='solid-line'></div>
+                    </Col>
                 </Row>
             </Container>
-            <Icon />
-            <div className='solid-line'></div>
         </div>
         </>
         );
