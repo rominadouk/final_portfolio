5d1a23f727408358be1038cc8580de5b9592cc7b
rominadouk <douk.romina@gmail.com>
2023-07-20 14:27:33 -0700

    successfully animated scroll down button, and disappears when user scrolls down with javascript function

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,18 +9,30 @@ const Intro = () => {
 
     const scrollDownButton = () => {
         const scrollButton = document.getElementById("scroll-down")
-        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            //Button will show if user is at the top of the window
-            scrollButton.style.display = "none" //Hide Button
-        } else {
+        if(window.scrollY < 20) {
             scrollButton.style.display = "block" //Show Button
+            scrollButton.classList.add("bounce") //Add bounce class if button is in view
+            scrollButton.classList.add("scroll-down-animation")
+
+        } else {
+        //Button will show if user scrolls down
+        scrollButton.style.display = "none" //Hide Button
+        scrollButton.classList.remove("bounce") //remove bounce class if button is in view
+        scrollButton.classList.remove("scroll-down-animation") //remove
 
     }
 }
 
     useEffect(()=> {
+        //calling initial function
         scrollDownButton();
-    }, [])
+
+        // Add an event listener to run function everytime user scrolls
+        window.addEventListener("scroll", () => {
+            scrollDownButton()
+        });
+        
+    },[])
 
     return ( 
         <>
@@ -40,7 +52,7 @@ const Intro = () => {
                 <Row className='scroll-row'>
                     <Col className='col-12'>
                         <Link to="projects" spy={true} smooth={true} offset={-90} duration={90}>
-                            <p className='scroll-down-txt' id='scroll-down'> <ArrowUp/> scroll down </p>
+                            <p className='scroll-down-txt' id='scroll-down'> scroll down <ArrowUp id="up-arrow"/> </p>
                         </Link>
                     </Col>
                 </Row>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
                             <div className="projectImg d-lg-none foodspot-img"></div>
                             <p className="project_Description"> A solo food blog project that is still being designed and built by me. This site was built with a mobile-first design in mind. Future updates will include a backend (Express JS) and a database (PostgreSQL). The website has a base design; functionality will be added shortly. </p>
                             <a href='https://foodspot-frontend.vercel.app/'>
-                            <button className="btn showProject">Show FoodSpot</button>
+                            <button className="btn showProject">Show App</button>
                             </a>
                             <a href='https://github.com/rominadouk/foodspot_frontend/tree/main'>
                             <button className="btn showProject showCode">Show Frontend Code</button>
@@ -39,7 +39,7 @@ const Projects = () => {
                             <div className="projectImg d-lg-none realist-img"></div>
                             <p className="project_Description"> A collaborative group project during my time at General Assembly. Our task was to build a Django API (Application Programming Interface) for our backend, and utilize React JS for our frontend, this project needed full CRUD functionality and must be able to display data. We decided on a Real Estate website, we learned Python and Tailwind CSS for this project. This homepage was designed by me. (* render.com may take a second to load)</p>
                             <a href='https://real-ist-frontend.onrender.com/' >
-                            <button className="btn showProject">Show Real-ist</button>
+                            <button className="btn showProject">Show App</button>
                             </a>
                             <a href='https://github.com/ryanmoon19/real-ist_frontend'>
                             <button className="btn showProject showCode">Show Frontend Code</button>
@@ -64,7 +64,7 @@ const Projects = () => {
                             <div className="projectImg d-lg-none zen-img"></div>
                             <p className="project_Description">My final capstone project to mark the completion of my time at General Assembly. I chose to hone my skills with the MERN stack. Zen is a mental health/self care application that helps users go more in depth about what they're feeling and in some instances, will provide users with resources based on the named emotions. Zen doubles as a journal, with the ability to save emotions that day and create journal posts. Try it out to find out more. </p>
                             <a href='https://zen-frontend.herokuapp.com/'>
-                                <button className="btn showProject">Show Zen</button>
+                                <button className="btn showProject">Show App</button>
                             </a>
                             <a href='https://github.com/rominadouk/zen_frontend'>
                                 <button className="btn showProject showCode">Show Frontend Code</button>
